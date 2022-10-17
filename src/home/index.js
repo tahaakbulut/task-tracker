31d10d751445043394297fcd8f60a2a29d9b68ac
tahaakbulut <34061497+tahaakbulut@users.noreply.github.com>
2022-10-17 04:01:22 +0300

    updated home

--- src/home/index.js
+++ src/home/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
 import Layout from '../components/layout';
+import { TasksProvider } from '../contexts/task-context';
+import TasksList from '../components/task-list';
+import CreateTask from '../components/task-create';
+import { Space } from 'antd';
 
-const Home = () => <Layout>Home</Layout>;
+const Home = () => (
+  <Layout>
+    <TasksProvider>
+      <Space direction="vertical" size="large" style={{ display: 'flex' }}>
+        <CreateTask />
+        <TasksList />
+      </Space>
+    </TasksProvider>
+  </Layout>
+);
 
 export default Home;
